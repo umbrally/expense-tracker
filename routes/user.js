54206323908c2da174dacbd4f768de8d212e3a4f
Jung <realumbrally@gmail.com>
2019-07-09 14:17:19 +0800

    Fix heroku error

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 const bcrypt = require('bcryptjs')
-const User = require('../models/user')
+const User = require('../models/user.js')
 
 // 登入頁面
 router.get('/login', (req, res) => {
